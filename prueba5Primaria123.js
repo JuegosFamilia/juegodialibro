881d8fcb8644d3620fa55540d45a4f4d48a6fb5c
CeciFullStack <ceciliaalarcond@hotmail.com>
2023-04-15 17:35:35 +0200

    Imagenes y más pruebas

---
+++ prueba5Primaria123.js
@@ -0,0 +1,12 @@
+
+  const respuestasCorrectas = ["MOONVILE"]; // Aquí puedes agregar más respuestas correctas
+    const verificarRespuestas = () => {
+      const respuesta1 = document.getElementById("respuesta1").value.toUpperCase();
+  
+      if (respuestasCorrectas.includes(respuesta1)) {
+        document.getElementById("respuestas-correctas").style.display = "block";
+        document.getElementById("imagen").src = "img/anna.jpeg"; // Aquí debes colocar la URL de tu imagen
+      } else {
+        alert("Inténtalo de nuevo.");
+      }
+    }
