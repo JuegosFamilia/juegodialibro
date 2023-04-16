8aafe5205bb6e9c2cdd236cbe10f41526c603783
CeciliaAD <ceciliaalarcond@hotmail.com>
2023-04-16 19:13:54 +0200

    Continúo con las pruebas

---
+++ prueba7Primaria456.js
@@ -0,0 +1,13 @@
+
+  const respuestasCorrectas = ["AMISTAD", "AMOR"]; // Aquí puedes agregar más respuestas correctas
+    const verificarRespuestas = () => {
+      const respuesta1 = document.getElementById("respuesta1").value.toUpperCase();
+      const respuesta2 = document.getElementById("respuesta2").value.toUpperCase();
+        
+      if (respuestasCorrectas.includes(respuesta1) && respuestasCorrectas.includes(respuesta2)) {
+        document.getElementById("respuestas-correctas").style.display = "block";
+        document.getElementById("imagen").src = "img/principito.jpeg"; // Aquí debes colocar la URL de tu imagen
+      } else {
+        alert("Inténtalo de nuevo.");
+      }
+    }
